5c9a2d944019c62d5fda0013cb7be9e6a5b4e092
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-12 15:54:22 +0000

    conversiondatatypes

---
+++ 01_Basics/01_conversionDatatypes.js
@@ -0,0 +1,30 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 'vaibhav';
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vaibhav" => true
+// null => false; 
+// undefined => false
+
+let someNumber = 44;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
