6923ae5500d8b0bc8c1670e007b934e273bce3fc
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-12 15:28:30 +0000

    datatypes

---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as Newer version
+
+// alert(3 + 3);  // We are using NodeJs not browser 
+
+console.log(3 
+    + 3)  // Code Readability should be high
+
+
+let name = "Vaibhav";
+let age = 18;
+let isLoggedIn = true;
+
+// Data Types
+// number => range 2 to the power 53
+// bigInt 
+// string => "" or ''
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined => It means value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
