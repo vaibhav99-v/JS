1c4745964698958b03c34f912e3f0ca3d7f8d3ac
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-15 14:26:02 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Strict check ===
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+
+console.log();
