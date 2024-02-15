3be88cfde10cfffa482b27f0704bfd910fc2d277
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-15 14:12:24 +0000

    conversion2

--- 01_Basics/01_conversionDatatypes.js
+++ 01_Basics/01_conversionDatatypes.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = 'vaibhav';
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,6 +25,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 44;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***********Operators***************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "Hello";
+let str2 = " Vaibhav";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(true);
+
+// console.log(+true);
+
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
 
