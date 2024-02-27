3730ea251c9ecf5273120642667208692ca59a30
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-27 10:44:32 +0000

    array

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,50 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+ 
+// console.log(myArr[0]);
+// console.log(myArr[1]);
+// console.log(myArr[2]);
+// console.log(myArr[3]);
+// console.log(myArr[4]);
+// console.log(myArr[5]);
+
+
+const myHeroes = ["shaktiman", "naagraj", "krrish"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr2[0]);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join();
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// Slice and Splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+
+console.log(myn2);
+
+console.log("C", myArr);
+
