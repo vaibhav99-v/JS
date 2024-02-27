3906d8d3597b0fd34a12ead3a728c47211daa2bf
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-02-27 07:29:06 +0000

    Nums

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -3,31 +3,31 @@ const repoCount = 50;
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('vai-bhav-99');
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(7));
-console.log(gameName.indexOf('a'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(7));
+console.log(gameName.indexOf('i'));
 
 const newString = gameName.substring(0,5);
-console.log(newString);
+// console.log(newString);
 
 const anotherString = gameName.slice(-11,4);
-console.log(anotherString);
+// console.log(anotherString);
 
 const newStringOne = "   vaibhav    ";
-console.log(newStringOne.trim());
+// console.log(newStringOne.trim());
 
 
 const url = "https://google.com/vaibhav%20v3009"
-console.log(url.replace('%20', '-'));
-console.log(url.includes('vaibhav'));
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('vaibhav'));
 
 
 console.log(gameName.split('-'));
---
+++ 01_Basics/06_Nums_and_math.js
@@ -0,0 +1,40 @@
+ const score = 400;
+// console.log(score);
+
+ const balance = new Number(100);
+//  console.log(balance)
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+ const otherNumber = 231.8396
+
+//  console.log(otherNumber.toPrecision(4));
+
+ const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // If you negative value it gives positive 
+// console.log(Math.round(4.6)); // It give round-off value
+// console.log(Math.ceil(4.2)); // It give lower value
+// console.log(Math.floor(4.7)); // It give top value
+// console.log(Math.min(4,5,3,2,7)); // It gives minimum value
+// console.log(Math.max(4,5,3,2,7)); // It gives maximum value
+
+// console.log(Math.random()); // Math.random() gives random values
+// console.log(Math.random()*10); 
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1); // If we want corerct value we can use this method
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log(Math.floor(Math.random()*(3 - 1 + 1)) + 1);
+
