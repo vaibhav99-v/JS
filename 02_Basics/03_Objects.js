31be815d2ff4df177a1c1fc6f82701afdc39e539
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-03-02 22:12:48 +0000

    Objects

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,43 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Vaibhav",
+    "full name": "Vaibhav Vishwakarma",
+    [mySym]: "key1",
+    age: 18,
+    location: "Pune",
+    email: "vaibhav99@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// Two ways to access the object key
+
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = "vaibhav.v@chatgpt.com"
+// Object.freeze(jsUser)
+jsUser.email = "vaibhav@microsoft.com"
+
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);  // String Interpolation
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
