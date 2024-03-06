2fce608f6551792c81bbd098ac3c2fec37aadd7f
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-03-06 19:04:41 +0000

    scope1

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -8,6 +8,32 @@ if (true) {
     var c = 40
 }
 
-console.log(a);   // let will throw an error
-console.log(b);    // const will throw an error
-console.log(c);     // var will print and but it is not a good thing
+// console.log(a);   // let will throw an error
+// console.log(b);    // const will throw an error
+// console.log(c);     // var will print and but it is not a good thing
+
+
+function one(){
+    const username = "vaibhav"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one()
+
+if (true) {
+    const username = "vaibhav"
+    if (username == "vaibhav") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+
+console.log(username);
