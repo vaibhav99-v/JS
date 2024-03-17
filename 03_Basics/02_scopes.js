be38550c1384319ec99997e5de2b3f40236bae69
Vaibhav Vishwakarma <76530483+vaibhav99-v@users.noreply.github.com>
2024-03-17 18:34:08 +0000

    scopes1

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -8,6 +8,11 @@ if (true) {
     var c = 40
 }
 
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
 // console.log(a);   // let will throw an error
 // console.log(b);    // const will throw an error
 // console.log(c);     // var will print and but it is not a good thing
@@ -25,7 +30,7 @@ function one(){
     two();
 }
 
-// one()
+one()
 
 if (true) {
     const username = "vaibhav"
@@ -33,7 +38,22 @@ if (true) {
         const website = "youtube"
         console.log(username + website);
     }
-    console.log(website);
+    // console.log(website);
 }
 
-console.log(username);
+// console.log(username);
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+
+// addTwo(5);   // It will throw an error like Cannot access before initialization
+
+const addTwo = function (num) {  // It is function or expression
+    return num + 2
+}
+
+
